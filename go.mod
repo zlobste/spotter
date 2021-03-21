12bba54a2e31a410c6da3b82de60bef746ea8850
zlobste <nkrayniuk@gmail.com>
2021-03-21 20:24:31 +0200

    Init project structure

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/zlobste/spotter
+
+go 1.15
+
+require (
+	github.com/alecthomas/kingpin v2.2.6+incompatible
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20210208195552-ff826a37aa15 // indirect
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.8.1
+	gopkg.in/yaml.v2 v2.4.0
+)
---
+++ internal/config/main.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+// Config defines an interface of global service configurations.
+type Config interface {
+	Logger() *logrus.Logger
+}
+
+// Config defines global service configurations.
+type config struct {
+	Log string `yaml:"log"`
+}
+
+// NewConfig returns global service configurations.
+func NewConfig(path string) Config {
+	cfg := config{}
+
+	yamlConfig, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(errors.Wrap(err, fmt.Sprintf("failed to read config: %s", path)))
+	}
+
+	err = yaml.Unmarshal(yamlConfig, &cfg)
+	if err != nil {
+		panic(errors.Wrap(err, fmt.Sprintf("failed to unmarshal config: %s", path)))
+	}
+
+	return &cfg
+}
+
+// Logger returns new configured logger.
+func (c *config) Logger() *logrus.Logger {
+	level, err := logrus.ParseLevel(c.Log)
+	if err != nil {
+		panic(errors.Wrapf(err, "failed to parse logging level %s", c.Log))
+	}
+
+	logger := logrus.New()
+	logger.SetLevel(level)
+
+	return logger
+}
