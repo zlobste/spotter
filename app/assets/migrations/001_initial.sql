ce4c895a1854519012577edd9637b25d77bfde9a
zlobste <nkrayniuk@gmail.com>
2021-04-15 23:51:50 +0300

    Update initial migration

--- app/assets/migrations/001_initial.sql
+++ app/assets/migrations/001_initial.sql
@@ -1,42 +1,15 @@
 -- +migrate Up
-CREATE TABLE blocks
+create table users
 (
-    id     BIGINT NOT NULL PRIMARY KEY,
-    hash   text   NOT NULL,
-    parent text   NOT NULL
+    id       bigserial,
+    name     text  not null default '',
+    surname  text  not null default '',
+    email    text  not null unique,
+    password text  not null,
+    role     int   not null default 2,
+    balance  money not null default 0,
+    PRIMARY KEY (id)
 );
-
-CREATE TABLE transactions
-(
-    hash      TEXT        NOT NULL PRIMARY KEY,
-    block_id  BIGINT      NOT NULL REFERENCES blocks (id) ON DELETE CASCADE,
-    sender    text        NOT NULL,
-    recipient text,
-    value     NUMERIC(32) NOT NULL,
-    nonce     BIGINT      NOT NULL,
-    gas_price NUMERIC(32) NOT NULL,
-    gas       BIGINT      NOT NULL,
-    input     bytea,
-    timestamp BIGINT      NOT NULL
-);
-
-CREATE INDEX tx_sender_idx ON transactions (sender);
-CREATE INDEX tx_recipient_idx ON transactions (recipient);
-
-CREATE TABLE payments
-(
-    tx_hash   TEXT        NOT NULL PRIMARY KEY REFERENCES transactions (hash) ON DELETE CASCADE,
-    sender    text        NOT NULL,
-    recipient text        NOT NULL,
-    token     text,
-    value     NUMERIC(32) NOT NULL,
-    timestamp BIGINT      NOT NULL
-);
-
-CREATE INDEX pay_sender_idx ON payments(sender);
-CREATE INDEX pay_recipient_idx ON payments(recipient);
 
 -- +migrate Down
-DROP TABLE payments;
-DROP TABLE transactions;
-DROP TABLE blocks;
+drop table users cascade;
